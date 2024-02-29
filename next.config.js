fd0188c2352654f479d9d990d0ff27fc12d0d3d2
pddhkt <pddhkt@gmail.com>
2024-03-01 02:26:41 +0800

    change to nextjs 14

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
+    images: {
+        // domains: ['qoueeudflsuricguegvb.supabase.co'],
+        // formats: ['image/avif', 'image/webp'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'img.clerk.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
 }
+
+// const withPWA = require('next-pwa')({
+//     dest: 'public',
+//     register: true,
+//     skipWaiting: true,
+// })
+
+// module.exports = withPWA({
+//     reactStrictMode: true,
+// })
 
 module.exports = nextConfig
